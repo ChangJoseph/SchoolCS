96f1484018c6d352f70dd7d012e50eb02c851b43
Joseph Chang <niceinho@gmail.com>
2019-01-26 17:23:40 -0500

    changed file paths

--- compressLZW2.py
+++ compressLZW2.py
@@ -1,6 +1,6 @@
 #This is a LZW compressor that reads the '.txt' file and compresses it
 import string
-textFile = open("longJoke.txt","r")
+textFile = open("toCompress.txt","r")
 writeLocation = "compressed.txt"
 stringText = textFile.read()
 textFile.close()
@@ -9,20 +9,20 @@ textFile.close()
 bits = 8
 
 
-dict = {}
+ogDict = {}
 
-#Initiates dictionary with: ascii characters as the dict key & its ascii value as the dict value
+#Initiates dictionary with: ascii characters as the ogDict key & its ascii value as the ogDict value
 for x in range(0,128):
-	dict[chr(x)] = x
+	ogDict[chr(x)] = x
 
-finalBinary = '' #Final binary string this script should outputs
+dict = ogDict #protects base ascii
 
 stringTextClone = stringText #Protects original text
 
 #This while loop determines the best amount of bits
 try:
 	while (len(stringTextClone) > 1): #while this string still has stuff in it
-		if (len(dict) > 2^bits): #if the dictionary length surpasses current bit size
+		if (len(dict) > 2**bits): #if the dictionary length surpasses current bit size
 			bits += 1
 		i=2
 		while (stringTextClone[0:i] in dict):
@@ -33,13 +33,43 @@ try:
 			dict[len(dict)]=stringTextClone[0:i]
 			stringTextClone = stringTextClone[i:]
 except:
-	print('error determining best bits')
+	print('error determining best bits; bits set to 10 as default')
 	bits = 10
 
 
-print(bits)
-input('')
+input('Best bits: ' + str(bits) + '\n')
 
+if (input('Do you want to continue? (y/n)\n').upper() != 'Y'):
+	quit()
+
+finalBinary = '' #Final binary string this script should outputs
+
+stringTextClone = stringText #Protects original text
+
+dict = ogDict #protects base ascii
+
+#This while loop will do the actual compressing
+try:
+	while (len(stringTextClone) > 1): #while this string still has stuff in it
+		while (stringTextClone[0:i] in dict):
+			i+=1
+		component = ''
+		if i > len(stringTextClone):
+			component = stringTextClone[i:]
+			dict[len(dict)]= component
+			stringTextClone = component
+		else:
+			component = stringTextClone[0:i]
+			dict[len(dict)]= component
+			stringTextClone = stringTextClone[i:]
+		finalBinary += bin(dict[component])[2:].rjust(bits,'0')
+except:
+	print('error compressing toCompress file\n')
+
+log = open(writeLocation,'w')
+log.write(finalBinary)
+log.close()
+input('Finished writing to ' + writeLocation +'\n')
 
 
 
